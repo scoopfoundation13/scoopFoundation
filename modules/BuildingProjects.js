dace5c625f80b4f4d3801252d422a48b49e02710
Wendy <tell.wendy@gmail.com>
2017-03-08 23:33:36 -0800

    add video montage, add buildingProjects mock up

--- modules/BuildingProjects.js
+++ modules/BuildingProjects.js
@@ -1,8 +1,50 @@
 import React from 'react';
+import ThreeARow from './ThreeARow';
 
 class BuildingProjects extends React.Component {
   render() {
-    return <div>BuildingProjects</div>
+    const imgArray = [{
+      img: 'modules/assets/img01.jpg',
+      link: '#project-cambodia',
+      text: 'Cambodia'
+    }, {
+      img: 'modules/assets/img02.jpg',
+      link: '#project-india',
+      text: 'India'
+    }, {
+      img: 'modules/assets/img03.jpg',
+      link: '#project-syria',
+      text: 'Syria'
+    }];
+    return (
+      <div className="container-topMargin">
+      <h2>BuildingProjects</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <section>
+        <ThreeARow img={imgArray}/>
+      </section>
+      <section className="project-cambodia">
+        <h2>Cambodia -  SCAO III</h2>
+        <p>
+        As you can see, the school is already half finished, but now we need to build the 2nd floor which will have more classrooms.
+        </p>
+        <p>
+        We also want to build quality accommodation to attract qualified teachers and Development Study graduates to come and live in village.
+        </p>
+        <p>
+        To get involved, get in touch with Andy today, make a donation (link to DONATE page) or set up a fundraising page (link to Just Giving Page) on our behalf.
+        </p>
+      </section>
+      <section className="project-india">
+        <h2>India</h2>
+        <p>India building projects content here...</p>
+      </section>
+      <section className="project-syria">
+        <h2>syria</h2>
+        <p>Syria building project content here...</p>
+      </section>
+      </div>
+      );
   }
 }
 
--- modules/Home.js
+++ modules/Home.js
@@ -30,7 +30,7 @@ class Home extends React.Component {
         <section className="home-sec1">
             <span>No Child should live in Poverty </span>
           <video autoPlay loop id="video-background" muted>
-            <source src="https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761" type="video/mp4"/>
+            <source src="modules/assets/INTR.mp4" type="video/mp4"/>
           </video>
         </section>
         <section className="home-sec2">
