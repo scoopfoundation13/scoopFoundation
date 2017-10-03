73e93913a3f963221031a43181e1c9959b463f0b
Chad Oakenfold <web@oakenfold.ca>
2017-10-03 11:00:23 -0700

    Change 2nd line of text on homepage main image #20

--- modules/Home.js
+++ modules/Home.js
@@ -182,7 +182,7 @@ class Home extends React.Component {
   <div style={{margin: "7%", padding: "1%"}}>
     <div style={{width:"48%", padding:"1%", color: "#fff"}}>
       <span className="home-text-sm" style={{}}>No child should live in poverty or danger.</span>
-      <p className="home-text" style={{}}>We raise funds in different ways so we can help as many lives as possible.</p>
+      <p className="home-text" style={{}}>We are on a mission to build a kinder world for children.</p>
     </div>
     <NavLink to="/donate" className="home-donate-btn">Donate</NavLink>
   </div>
